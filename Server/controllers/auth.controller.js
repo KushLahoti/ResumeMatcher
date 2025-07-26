684b7c8855098f2d8cc2fcbf158ae097e04b1cc3
sri vikhil <vikhil1912@gmail.com>
2025-07-26 17:12:05 +0530

    complete test student res upload

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,136 +1,147 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from '../models/User.model.js';
-import RefreshToken from '../models/refreshToken.model.js';
+import User from "../models/User.model.js";
+import RefreshToken from "../models/refreshToken.model.js";
 
 const max_age_access_token = parseInt(process.env.MAX_AGE_ACCESS_TOKEN);
 const max_age_refresh_token = parseInt(process.env.MAX_AGE_REFRESH_TOKEN);
 
 const createToken = (user) => {
-    const payload = {
-        userId: user._id,
-        role: user.role
-    }
+  const payload = {
+    userId: user._id,
+    role: user.role,
+  };
 
-    const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
-    const refreshToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+  });
+  const refreshToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  });
 
-    return { accessToken, refreshToken };
-}
+  return { accessToken, refreshToken };
+};
 
 const setCookies = (res, accessToken, refreshToken) => {
-    res.cookie('accessToken', accessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        maxAge: max_age_access_token
-    })
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    maxAge: max_age_access_token,
+  });
 
-    res.cookie('refreshToken', refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        maxAge: max_age_refresh_token
-    })
-}
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    maxAge: max_age_refresh_token,
+  });
+};
 
 export const signUpUser = async (req, res) => {
-    const { name, email, password, role } = req.body;
-    if (!name || !email || !password || !role) {
-        return res.status(400).send({ success: false, message: "Enter all the fields" });
+  const { name, email, password, role } = req.body;
+  if (!name || !email || !password || !role) {
+    return res
+      .status(400)
+      .send({ success: false, message: "Enter all the fields" });
+  }
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(409)
+        .send({ success: false, message: "User Already Exist" });
     }
-    try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(409).send({ success: false, message: 'User Already Exist' });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({
-            name,
-            email,
-            password: hashedPassword,
-            role
-        })
-        await user.save();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role,
+    });
+    await user.save();
 
-        const { accessToken, refreshToken } = createToken(user);
+    const { accessToken, refreshToken } = createToken(user);
 
-        setCookies(res, accessToken, refreshToken);
+    setCookies(res, accessToken, refreshToken);
 
-        await RefreshToken.create({
-            refreshToken,
-            userId: user._id
-        })
+    await RefreshToken.create({
+      refreshToken,
+      userId: user._id,
+    });
 
-        return res.status(201).send({
-            success: true,
-            message: "User created successfully"
-        })
-
-    } catch (error) {
-        console.error('Error from SignUpController', error);
-        res.send({ success: false, message: error.message });
-    }
-}
+    return res.status(201).send({
+      success: true,
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.error("Error from SignUpController", error);
+    res.send({ success: false, message: error.message });
+  }
+};
 
 export const login = async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-        return res.status(401).send({ success: false, message: "Missing credentials" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(401)
+      .send({ success: false, message: "Missing credentials" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401).send({ success: false, message: "Invalid email" });
     }
-    try {
-        const user = await User.findOne({ email });
-        if (!user) {
-            res.status(401).send({ success: false, message: "Invalid email" });
-        }
-
-        const isPasswordMatching = await bcrypt.compare(password, user.password);
-        if (!isPasswordMatching) {
-            res.status(401).send({ success: false, message: "Invalid password" });
-        }
-
-        const { accessToken, refreshToken } = createToken(user);
-        setCookies(res, accessToken, refreshToken);
-
-        await RefreshToken.create({
-            refreshToken,
-            userId: user._id
-        });
-
-        return res.status(201).send({
-            success: true,
-            message: "Login Succesful"
-        })
-    } catch (error) {
-        console.error('Error from LoginController', error);
-        res.status(500).send({ success: false, message: error.message });
+    const refToken = await RefreshToken.find({ userId: user._id });
+    if (refToken.length > 0) {
+      await RefreshToken.deleteMany({ userId: user._id });
     }
-}
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatching) {
+      res.status(401).send({ success: false, message: "Invalid password" });
+    }
+
+    const { accessToken, refreshToken } = createToken(user);
+    setCookies(res, accessToken, refreshToken);
+
+    await RefreshToken.create({
+      refreshToken,
+      userId: user._id,
+    });
+
+    return res.status(201).send({
+      success: true,
+      message: "Login Succesful",
+    });
+  } catch (error) {
+    console.error("Error from LoginController", error);
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
 
 export const logout = async (req, res) => {
-    try {
+  try {
+    const { refreshToken } = req.cookies;
 
-        const { refreshToken } = req.cookies;
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: max_age_access_token,
+    });
 
-        res.clearCookie('accessToken', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            maxAge: max_age_access_token
-        })
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: max_age_access_token,
+    });
 
-        res.clearCookie('refreshToken', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            maxAge: max_age_access_token
-        })
+    await RefreshToken.findOneAndDelete({ refreshToken });
 
-        await RefreshToken.findOneAndDelete({ refreshToken });
-
-        return res.status(201).send({ success: true, message: 'Logged Out' });
-    } catch (error) {
-        console.error('Error from LogoutController', error);
-        res.status(500).send({ success: false, message: error.message });
-    }
-}
+    return res.status(201).send({ success: true, message: "Logged Out" });
+  } catch (error) {
+    console.error("Error from LogoutController", error);
+    res.status(500).send({ success: false, message: error.message });
+  }
+};
